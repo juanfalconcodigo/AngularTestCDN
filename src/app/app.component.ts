94d80ebeebf45f15639be4b7cfb212f41c8bbbe4
JUAN FALCON <JUANCANEN_10_@HOTMAIL.COM>
2021-06-23 02:05:05 -0500

    Added project angular version 11 test execute CDN

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular10app';
+  cdnUrl = "https://cdn.jsdelivr.net/gh/juanfalconcodigo/CDN_TEST@1.0/test.js";
+  executeJs() {
+    console.log('execute script');
+    let me = this;
+    const node = document.createElement('script');
+    node.src = me.cdnUrl;
+    node.type = 'text/javascript';
+    node.async = false;
+    document.getElementsByTagName('head')[0].appendChild(node);
+  }
 }
